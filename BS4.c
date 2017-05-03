abfe80faeae859cbfa967de40a7337810ef290a3
Simon Rüba <simonrueba@gmail.com>
2017-05-03 12:49:43 +0200

    Update BS4.c

--- BS4.c
+++ BS4.c
@@ -4,6 +4,12 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+/*	
+	Write a program in which the parent creates exactly 1 child process. The child process should print its 
+	pid to the standard output and then finish. The parent process waits until it is sure that the child has 
+	terminated. For this students are required to check in the man page of fork to see whether a parent is notified 
+	of child termination via any signal. The parent terminates after it has waited for the child process.
+*/
 
 int main(int argc, char* argv[])
 {
